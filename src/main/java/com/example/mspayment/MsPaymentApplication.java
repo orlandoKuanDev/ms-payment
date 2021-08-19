c0e74158a2ec7e05acbe29a9936ad7da169a60f5
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-08-19 11:58:16 -0500

    FIX adding eureka client

--- src/main/java/com/example/mspayment/MsPaymentApplication.java
+++ src/main/java/com/example/mspayment/MsPaymentApplication.java
@@ -2,7 +2,9 @@ package com.example.mspayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsPaymentApplication {
 
